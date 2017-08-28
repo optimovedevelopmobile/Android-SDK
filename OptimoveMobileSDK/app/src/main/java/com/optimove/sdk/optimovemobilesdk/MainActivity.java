59d496db62f33823e6396cf97e86eff2c94f30e7
Noy Grisaru <noy_g@optimove.com>
2017-08-28 10:27:32 +0300

    Test Activity UI updates for Firebase TestLab

--- OptimoveMobileSDK/app/src/main/java/com/optimove/sdk/optimovemobilesdk/MainActivity.java
+++ OptimoveMobileSDK/app/src/main/java/com/optimove/sdk/optimovemobilesdk/MainActivity.java
@@ -3,6 +3,7 @@ package com.optimove.sdk.optimovemobilesdk;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.optimove.sdk.optimove_sdk.main.Optimove;
 import com.optimove.sdk.optimove_sdk.main.entities.OptimoveEvent;
@@ -16,18 +17,23 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView outputTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        outputTv = (TextView) findViewById(R.id.userIdTextView);
     }
 
     public void reportEvent(View view) {
 
+        outputTv.setText("Reporting event");
         Optimove.getInstance().reportEvent(new MyEvent(), new OptimoveEventSentListener() {
             @Override
             public void onResponse(EventSentResult result) {
-                OptiLogger.d(this, result.name());
+                OptiLogger.d(this, "Report event result: %s", result.name());
             }
         });
         OptiTrackManager optiTrackManager = Optimove.getInstance().getOptiTrackManager();
@@ -38,11 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateUserId(View view) {
 
+        outputTv.setText("Setting userId");
         String userId = Optimove.getInstance().getUserInfo().getUserId();
         if (userId == null)
             Optimove.getInstance().setUserId("TestUserID");
         else
             Optimove.getInstance().setUserId(null);
+        reportEvent(null);
     }
 
     private class MyEvent implements OptimoveEvent {
